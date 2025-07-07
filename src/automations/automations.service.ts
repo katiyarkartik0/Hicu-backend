54d71dbff858d77489d063a78b27e8afbf09a17f
katiyarkartik0@gmail.com <kartik.katiyar@newtonschool.co>
2025-07-07 13:54:14 +0530

    cleanup1

--- src/automations/automations.service.ts
+++ src/automations/automations.service.ts
@@ -3,7 +3,6 @@ import { CreateAutomationDto } from './dto/create-automation.dto';
 import { UpdateAutomationDto } from './dto/update-automation.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import type {
-  Automation,
   IgCommentAutomation,
   IgDmAutomation,
 } from './automations.types';
@@ -11,28 +10,6 @@ import type {
 @Injectable()
 export class AutomationsService {
   constructor(private readonly prismaService: PrismaService) {}
-  create(createAutomationDto: Omit<Automation, 'id'>) {
-  }
-
-  findAll({ accountId }: { accountId: number }) {
-  }
-
-  findOne(id: number) {
-  }
-
-  findByMedia(mediaId: string) {
-  }
-
-  update(id: number, updateAutomationDto: UpdateAutomationDto) {
-    return `This action updates a #${id} automation`;
-  }
-
-  async remove(id: number) {
-    // await this.prismaService.userProgress.deleteMany({
-    //   where: { automationId: id },
-    // });
-    // return this.prismaService.automation.delete({ where: { id } });
-  }
 
   createIgCommentAutomation(data: Omit<IgCommentAutomation, 'id'>) {
     return this.prismaService.igCommentAutomation.create({ data });
